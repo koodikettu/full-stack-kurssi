610504d1c8004b0d6f657d54f1e5c1c1e4119590
koodikettu <markku.laakso@helsinki.fi>
2018-01-30 22:23:53 +0200

    harj 1.6

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    constructor() {
+      super()
+      this.state = {
+        hyva: 0,
+        neutraali: 0,
+        huono: 0
+      }
+    }
+
+    hyva = () => {
+        this.setState({ hyva: this.state.hyva + 1})
+    }
+
+    neutraali = () => {
+        this.setState({ neutraali: this.state.neutraali + 1})
+    }
+
+    huono = () => {
+        this.setState({ huono: this.state.huono + 1})
+    }
+
+
+    render(){
+      return (
+        <div>
+            <button onClick={this.hyva}> hyvä </button>
+            <button onClick={this.neutraali}> neutraali </button>
+            <button onClick={this.huono}> huono </button>
+            <h2> Tulokset </h2>
+          Hyvä: { this.state.hyva } <br />
+          Neutraali: { this.state.neutraali } <br />
+          Huono: { this.state.huono } <br />
+        </div>
+      )
+    }
+  }
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
