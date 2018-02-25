18b0d0f3d196e167327eda599a4b4ec0977914dc
koodikettu <markku.laakso@helsinki.fi>
2018-02-25 16:46:26 +0200

    Osa 4 teht 4.4

--- osa4/blogilista/utils/listHelpers.js
+++ osa4/blogilista/utils/listHelpers.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const reducer = (accumulator, currentValue) => {
+        return accumulator + currentValue.likes
+    }
+    return blogs.length > 0 ? blogs.reduce(reducer, 0) : 0
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
