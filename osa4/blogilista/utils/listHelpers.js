6868211ad3bb24ce001c33f10fe54279fea4da11
koodikettu <markku.laakso@helsinki.fi>
2018-02-25 17:11:49 +0200

    Osa 4 teht 4.6

--- osa4/blogilista/utils/listHelpers.js
+++ osa4/blogilista/utils/listHelpers.js
@@ -9,7 +9,34 @@ const totalLikes = (blogs) => {
     return blogs.length > 0 ? blogs.reduce(reducer, 0) : 0
 }
 
+const favoriteBlog = (blogs) => {
+    const compareFunc = (a, b) => {
+        return b.likes - a.likes
+    }
+    return blogs.sort(compareFunc)[0]
+}
+
+const mostBlogs = (blogs) => {
+    const blogsByAuthor = {}
+    blogs.forEach(blog => {
+        if (blogsByAuthor[blog.author]) {
+            blogsByAuthor[blog.author].blogs++
+        } else {
+            blogsByAuthor[blog.author] = {
+                author: blog.author,
+                blogs: 1
+            }
+        }
+    })
+    const compareFunc = (a, b) => b.blogs - a.blogs
+    return Object.values(blogsByAuthor).sort(compareFunc)[0]
+    
+
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
